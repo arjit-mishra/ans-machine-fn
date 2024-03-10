6a1a5db96ac9a6094befde72ba0e80fdeda14c9a
Arjit Mishra <arjitm787@gmail.com>
2024-03-10 10:05:10 +0530

    init open AI

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,34 @@
-import { Client } from 'node-appwrite';
+import {Configuration, OpenAIApi} from 'openai';
+import {throwIfMissing} from "./utils";
 
-// This is your Appwrite function
-// It's executed each time we get a request
-export default async ({ req, res, log, error }: any) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(Bun.env["APPWRITE_FUNCTION_PROJECT_ID"])
-  //    .setKey(Bun.env["APPWRITE_API_KEY"]);
+export default async ({req, res, log, error}) => {
+  throwIfMissing(process.env, ['OPENAI_API_KEY']);
 
-  // You can log messages to the console
-  log("Hello, Logs!");
-
-  // If something goes wrong, log an error
-  error("Hello, Errors!");
-
-  // The `req` object contains the request data
-  if (req.method === "GET") {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send("Hello, World!");
+  try {
+    throwIfMissing(req.body, ['prompt']);
+  } catch (err) {
+    error("Invalid request in check ", err);
+    return res.json({ok: false, error: err.message}, 400);
   }
 
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
+  const openai = new OpenAIApi(new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  }));
+
+  if (req.method === 'GET') {
+    try {
+      const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS ?? '512'),
+        messages: [{role: 'user', content: req.body.prompt}],
+      });
+      const completion = response.data.choices[0].message?.content;
+      log(completion);
+      return res.json({ok: true, completion}, 200);
+    } catch (err) {
+      error("Invalid request in get ", err);
+      return res.json({ok: false, error: 'Failed to query model.'}, 500);
+    }
+  }
+
 };
---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+import * as path from 'path';
+
+const staticFolder = path.join(__dirname, '../static');
+
+
+/**
+ * Throws an error if any of the keys are missing from the object
+ * @param {*} obj
+ * @param {string[]} keys
+ * @throws {Error}
+ */
+export function throwIfMissing(obj, keys) {
+  const missing = [];
+  for (let key of keys) {
+    if (!(key in obj) || !obj[key]) {
+      missing.push(key);
+    }
+  }
+  if (missing.length > 0) {
+    throw new Error(`Missing required fields: ${missing.join(', ')}`);
+  }
+}
